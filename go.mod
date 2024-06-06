eb79b3dd7c9b0ef7792ccd357a93d8b36cdc58db
Andrew Snodgrass <pointlander@gmail.com>
2024-06-06 18:30:26 -0500

    WIP self attention transform

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/pointlander/kan
 
 go 1.19
 
-require github.com/mjibson/go-dsp v0.0.0-20180508042940-11479a337f12 // indirect
+require (
+	github.com/mjibson/go-dsp v0.0.0-20180508042940-11479a337f12 // indirect
+	github.com/pointlander/matrix v0.0.0-20240501200500-3225dbdfbf15 // indirect
+)
--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/mjibson/go-dsp/dsputils"
 	"github.com/mjibson/go-dsp/fft"
+	"github.com/pointlander/matrix"
 )
 
 // Useses the fft to resize an image
@@ -26,11 +27,11 @@ func Resize(gray *image.Gray, xscale, yscale, padx, pady int) *image.Gray {
 	mat := dsputils.MakeMatrix(make([]complex128, xscale*yscale*((dx/xscale)*(dy/yscale))),
 		[]int{xscale, yscale, dx / xscale, dy / yscale})
 	for x := 0; x < dx; x += xscale {
-		for y := 0; y < dy; y += 8 {
+		for y := 0; y < dy; y += yscale {
 			for a := 0; a < xscale; a++ {
-				for b := 0; b < 8; b++ {
+				for b := 0; b < yscale; b++ {
 					g := float64(gray.GrayAt(x+a, y+b).Y)
-					mat.SetValue(complex(g/255, 0), []int{a, b, x / xscale, y / 8})
+					mat.SetValue(complex(g/255, 0), []int{a, b, x / xscale, y / yscale})
 				}
 			}
 		}
@@ -79,6 +80,102 @@ func Resize(gray *image.Gray, xscale, yscale, padx, pady int) *image.Gray {
 	return out
 }
 
+// Transform uses fft and self attention to transform an image
+func Transform(gray *image.Gray, xscale, yscale, padx, pady int) *image.Gray {
+	bounds := gray.Bounds()
+	dx := bounds.Dx()
+	dy := bounds.Dy()
+	fmt.Println(dx/xscale, dy/yscale)
+	mat := dsputils.MakeMatrix(make([]complex128, xscale*yscale*((dx/xscale)*(dy/yscale))),
+		[]int{xscale, yscale, dx / xscale, dy / yscale})
+	for x := 0; x < dx; x += xscale {
+		for y := 0; y < dy; y += yscale {
+			for a := 0; a < xscale; a++ {
+				for b := 0; b < yscale; b++ {
+					g := float64(gray.GrayAt(x+a, y+b).Y)
+					mat.SetValue(complex(g/255, 0), []int{a, b, x / xscale, y / yscale})
+				}
+			}
+		}
+	}
+	freq := fft.FFTN(mat)
+	mat3 := matrix.NewMatrix(xscale*yscale, ((dx / xscale) * (dy / yscale)))
+	for x := 0; x < dx/xscale; x++ {
+		for y := 0; y < dy/yscale; y++ {
+			for a := 0; a < xscale; a++ {
+				for b := 0; b < yscale; b++ {
+					value := freq.Value([]int{a, b, x, y})
+					mat3.Data = append(mat3.Data, float32(cmplx.Abs(value)))
+				}
+			}
+		}
+	}
+	mat4 := matrix.SelfAttention(mat3, mat3, mat3)
+	mat2 := dsputils.MakeMatrix(make([]complex128, xscale*yscale*((dx/xscale)*(dy/yscale))),
+		[]int{xscale, yscale, dx / xscale, dy / yscale})
+	for x := 0; x < dx/xscale; x++ {
+		for y := 0; y < dy/yscale; y++ {
+			value := mat4.Data[(y*(dy/yscale)+x)*yscale*xscale]
+			mat2.SetValue(complex(float64(value), 0), []int{0, 0, x, y})
+		}
+	}
+	inverse := fft.IFFTN(mat2)
+	out := image.NewGray(image.Rect(0, 0, dx/xscale+padx, dy/yscale+pady))
+	min, max := make([]float64, xscale*yscale), make([]float64, xscale*yscale)
+	for i := range min {
+		min[i] = 255
+	}
+	for x := 0; x < dx/xscale; x++ {
+		for y := 0; y < dy/yscale; y++ {
+			index := 0
+			for a := 0; a < xscale; a++ {
+				for b := 0; b < yscale; b++ {
+					vv := inverse.Value([]int{a, b, x, y})
+					vvalue := 255 * cmplx.Abs(vv)
+					/*if value != vvalue {
+						fmt.Println("not the same")
+					}*/
+					if vvalue < min[index] {
+						min[index] = vvalue
+					}
+					if vvalue > max[index] {
+						max[index] = vvalue
+					}
+					index++
+				}
+			}
+		}
+	}
+	fmt.Println(min)
+	fmt.Println(max)
+
+	for x := 0; x < dx/xscale; x++ {
+		for y := 0; y < dy/yscale; y++ {
+			v := inverse.Value([]int{0, 0, x, y})
+			value := 255 * cmplx.Abs(v)
+			index := 0
+			for a := 0; a < xscale; a++ {
+				for b := 0; b < yscale; b++ {
+					vv := inverse.Value([]int{a, b, x, y})
+					vvalue := 255 * cmplx.Abs(vv)
+					/*if value != vvalue {
+						fmt.Println("not the same")
+					}*/
+					if vvalue < min[index] {
+						min[index] = vvalue
+					}
+					if vvalue > max[index] {
+						max[index] = vvalue
+					}
+					index++
+				}
+			}
+			out.SetGray(x, y, color.Gray{Y: byte(255 * value / max[0])})
+		}
+	}
+	return out
+}
+
 func main() {
 	input, err := os.Open("test.jpg")
 	if err != nil {
@@ -105,6 +202,16 @@ func main() {
 	}
 	defer output.Close()
 	err = png.Encode(output, out)
+	if err != nil {
+		panic(err)
+	}
+	out2 := Transform(out, 16, 16, 0, 0)
+	output2, err := os.Create("transformed.jpg")
+	if err != nil {
+		panic(err)
+	}
+	defer output.Close()
+	err = png.Encode(output2, out2)
 	if err != nil {
 		panic(err)
 	}
